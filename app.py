b91a874b054c12140da21039c8dd81ac0bcab099
IaMGeorgious <iqbalabdulmajid03@gmail.com>
2024-03-26 15:39:54 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://iqbal:iqbal@cluster0.ie0bayw.mongodb.net/')
-db = client.dbWistlist
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+MONGODB_URI = os.environ.get('mongodb+srv://iqbal:iqbal@cluster0.ie0bayw.mongodb.net/')
+DB_NAME =  os.environ.get('dbWistlist')
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
